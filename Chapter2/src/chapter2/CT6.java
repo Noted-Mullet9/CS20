95edddc5700f3449a9cb1187a48a3e6ca6c43409
64278001 <64278001@L815WD150050.edu.cbe.ab.ca>
2022-03-07 12:25:04 -0700

    Chapter 2 Everyhting Updated

--- Chapter2/src/chapter2/CT6.java
+++ Chapter2/src/chapter2/CT6.java
@@ -2,9 +2,9 @@ package chapter2;
 
  /*
 
- Program: CT5.java          Last Date of this Revision: March 7, 2022
+ Program: CT6.java          Last Date of this Revision: March 7, 2022
 
- Purpose: Critical thinking question 5 on page #46
+ Purpose: Critical thinking question 6 on page #46
 
  Author: Alador Tesema, 
  School: CHHS
@@ -12,7 +12,7 @@ package chapter2;
  
  */
 
- public class CT5
+ public class CT6
 
  {
      public static void main(String[] args)
