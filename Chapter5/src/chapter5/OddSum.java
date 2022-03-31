97174a8497f3772868e8158f54d356eb0578465b
64278001 <64278001@L815WD220015.edu.cbe.ab.ca>
2022-03-31 11:12:12 -0600

    chapter5

--- Chapter5/src/chapter5/OddSum.java
+++ Chapter5/src/chapter5/OddSum.java
@@ -1,5 +1,12 @@
+
 package chapter5;
+
+import java.util.Scanner;
 
 public class OddSum {
 
-}
+  public static void main(String[] args) {
+
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("Enter a number: ");
