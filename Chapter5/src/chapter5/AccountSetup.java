50de9d30573327a8ade585d43ce1ff60d2e23005
64278001 <64278001@L815WD220015.edu.cbe.ab.ca>
2022-03-31 12:01:10 -0600

    Chapter5 EVERYTHING UPDATED

--- Chapter5/src/chapter5/AccountSetup.java
+++ Chapter5/src/chapter5/AccountSetup.java
@@ -1,5 +1,35 @@
 package chapter5;
 
+import java.util.Scanner;
+
 public class AccountSetup {
 
+  public static void main(String[] args) {
+
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("Enter a username: ");
+
+    String username = input.nextLine();
+    
+    System.out.println("Enter a username: ");
+
+    String password = input.nextLine();
+    
+    username = username.toLowerCase();
+    
+    password = password.toLowerCase();
+    
+    System.out.println("Your entered username is: " + username);
+    
+    System.out.println("Your entered pass is: " + password);
+
+    
+    
+    
+    
+    
+    
+  }
+  
 }
