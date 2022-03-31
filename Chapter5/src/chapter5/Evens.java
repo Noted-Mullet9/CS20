da3ddc785b1c9550e22be5a7f347acacdd41af46
64278001 <64278001@L815WD220015.edu.cbe.ab.ca>
2022-03-31 11:10:18 -0600

    Chapter 5 CREATED AMD EVERYHTING UPDATED

---
+++ Chapter5/src/chapter5/Evens.java
@@ -0,0 +1,20 @@
+package chapter5;
+
+public class Evens {
+
+  public static void main(String[] args) {
+
+    System.out.println("Even numbers between 1 and 20: ");
+
+int num = 0; 
+
+while (num < 20 ) { 
+	
+	num+=2; 
+	
+	System.out.println(num);
+	
+  }
+
+} 
+}
