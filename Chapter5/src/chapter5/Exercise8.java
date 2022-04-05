3591062f6b63abe33aa7604b0a191e1067b037c6
64278001 <64278001@L815WD220015.edu.cbe.ab.ca>
2022-04-05 11:51:41 -0600

    CHAPTER 5 EVERYTHING UPDATED

--- Chapter5/src/chapter5/Exercise8.java
+++ Chapter5/src/chapter5/Exercise8.java
@@ -22,18 +22,32 @@
      
      {
     	 
-    	 int Random = Num1(Math.random();
-    	 
-         //prompts a user for a 2 digit number
-         System.out.print("Enter a number between 1 and 20: ");
+    	 int max = 20;
+    	 int min = 1;
+    	 int number = 0;
+    			 
+    	 int random = (int)(Math.random()*(max-min+1)+min);
+         
+         do {
+        	 
+        	 //prompts a user for a 2 digit number
+             System.out.print("Enter a number between 1 and 20: ");
 
-         //records user input
-         int number = input.nextInt();
+             //records user input
+              number = input.nextInt();
+              
+         if (number != random) {
+              
+              System.out.println("Sorry you guessed wrong. \n");
+         }
+        	}
+        	
+         while (number != random);
 
+        	  }
+         
+         System.out.println("Congrats you guessed correct! ");
+         
+         
        }
-
-     }
-     	
-   } 
-
-
+ }
