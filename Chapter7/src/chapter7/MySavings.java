8e436687355f213653cc8c7dc55074035f17aa71
64278001 <64278001@L815WD220020.edu.cbe.ab.ca>
2022-04-20 10:26:47 -0600

    Chapter 7 NOTHING DONE BRUH

--- Chapter7/src/chapter7/MySavings.java
+++ Chapter7/src/chapter7/MySavings.java
@@ -10,143 +10,118 @@ import java.awt.event.ActionEvent;
 
 import javax.swing.JTextField;
 import javax.swing.JButton;
+import java.awt.event.ActionListener;
 
 public class MySavings {
 
-	private JFrame frame;
-	private JTextField input;
-	private int piggyBank;
+  private JFrame frame;
+  private JTextField input;
+  private int piggyBank;
+  private JLabel output;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					MySavings window = new MySavings();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+  int newTotal;
 
-	/**
-	 * Create the application.
-	 */
-	public MySavings() {
-		initialize();
-	}
+  /**
+   * Launch the application.
+   */
+  public static void main(String[] args) {
+    EventQueue.invokeLater(new Runnable() {
+      public void run() {
+        try {
+          MySavings window = new MySavings();
+          window.frame.setVisible(true);
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
+      }
+    });
+  }
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
-		
-		JPanel panel = new JPanel();
-		panel.setBounds(0, 0, 434, 261);
-		frame.getContentPane().add(panel);
-		panel.setLayout(null);
-		
-		JLabel p1 = new JLabel("1. Show Total in bank.");
-		p1.setBounds(10, 62, 185, 14);
-		panel.add(p1);
-		
-		JLabel p2 = new JLabel("2. Add a penny.");
-		p2.setBounds(10, 87, 131, 14);
-		panel.add(p2);
-		
-		JLabel p0 = new JLabel("Savings");
-		p0.setBounds(180, 0, 84, 48);
-		panel.add(p0);
-		p0.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		
-		JLabel p5 = new JLabel("5. Add a quarter.");
-		p5.setBounds(10, 162, 113, 14);
-		panel.add(p5);
-		
-		JLabel p3 = new JLabel("3. Add a nickel.");
-		p3.setBounds(10, 112, 113, 14);
-		panel.add(p3);
-		
-		JLabel p4 = new JLabel("4. Add a dime.");
-		p4.setBounds(10, 137, 93, 14);
-		panel.add(p4);
-		
-		JLabel p6 = new JLabel("6. Take money out of bank");
-		p6.setBounds(10, 177, 213, 36);
-		panel.add(p6);
-		
-		JLabel p7 = new JLabel("Enter 0 to quit");
-		p7.setBounds(298, 87, 94, 14);
-		panel.add(p7);
-		
-		JLabel p8 = new JLabel("Enter your choice");
-		p8.setBounds(298, 112, 113, 14);
-		panel.add(p8);
-		
-		input = new JTextField();
-		input.setBounds(280, 141, 131, 56);
-		panel.add(input);
-		input.setColumns(10);
-		
-		JButton Submit = new JButton("Submit");
-		Submit.setBounds(303, 208, 89, 23);
-		panel.add(Submit);
-		
-		piggyBank = 0;
-	}
-	
-	public void actionPerformed(ActionEvent e) {
-		
-		int userinput = Integer.parseInt(input.getText());
-		
-		switch (userinput) {
+  /**
+   * Create the application.
+   */
+  public MySavings() {
+    initialize();
+  }
 
-	    case 1:
+  /**
+   * Initialize the contents of the frame.
+   */
+  private void initialize() {
+    frame = new JFrame();
+    frame.setBounds(100, 100, 450, 300);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.getContentPane().setLayout(null);
 
-	      System.out.println("\nCategory 1: 74-95 mph or 64-82 kt or 119-153 km/hr");
+    JPanel panel = new JPanel();
+    panel.setBounds(0, 0, 434, 261);
+    frame.getContentPane().add(panel);
+    panel.setLayout(null);
 
-	      break;
+    JLabel p1 = new JLabel("1. Show Total in bank.");
+    p1.setBounds(10, 62, 185, 14);
+    panel.add(p1);
 
-	    case 2:
+    JLabel p2 = new JLabel("2. Add a penny.");
+    p2.setBounds(10, 87, 131, 14);
+    panel.add(p2);
 
-	      System.out.println("\nCategory 2: 96-110 mph or 83-95 kt or 154-177 km/hr");
+    JLabel p0 = new JLabel("Savings");
+    p0.setBounds(180, 0, 84, 48);
+    panel.add(p0);
+    p0.setFont(new Font("Tahoma", Font.PLAIN, 20));
 
-	      break;
+    JLabel p5 = new JLabel("5. Add a quarter.");
+    p5.setBounds(10, 162, 113, 14);
+    panel.add(p5);
 
-	    case 3:
+    JLabel p3 = new JLabel("3. Add a nickel.");
+    p3.setBounds(10, 112, 113, 14);
+    panel.add(p3);
 
-	      System.out.println("\nCategory 3: 111-130 mph or 96-113 kt or 178-209 km/hr");
+    JLabel p4 = new JLabel("4. Add a dime.");
+    p4.setBounds(10, 137, 93, 14);
+    panel.add(p4);
 
-	      break;
+    JLabel p6 = new JLabel("6. Take money out of bank");
+    p6.setBounds(10, 177, 213, 36);
+    panel.add(p6);
 
-	    case 4:
+    JLabel p7 = new JLabel("Enter 0 to quit");
+    p7.setBounds(298, 87, 94, 14);
+    panel.add(p7);
 
-	      System.out.println("\nCategory 4: 131-155 mph or 113-155 kt or 210-249 km/hr");
+    JLabel p8 = new JLabel("Enter your choice");
+    p8.setBounds(298, 112, 113, 14);
+    panel.add(p8);
 
-	      break;
+    input = new JTextField();
+    input.setBounds(280, 141, 131, 56);
+    panel.add(input);
+    input.setColumns(10);
 
-	    case 5:
+    JButton Submit = new JButton("Submit");
+    Submit.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
 
-	      System.out.println("\nCategory 5: greater than 155 mph or 135 kt or 249 km/hr");
+        int userinput = Integer.parseInt(input.getText());
 
-	      break;
-	      
-	    case 6:
+          output.setText(String.valueOf(newTotal));
+          // System.out.println(String.valueOf(piggyBank));
+          
+          
 
-		      System.out.println("\nCategory 5: greater than 155 mph or 135 kt or 249 km/hr");
+      }
 
-		      break;
+    });
+    Submit.setBounds(303, 208, 89, 23);
+    panel.add(Submit);
 
-	    }
+    output = new JLabel("");
+    output.setBounds(10, 208, 275, 42);
+    panel.add(output);
 
-	  }
-	}
+    piggyBank = 0;
+  }
 
+}
