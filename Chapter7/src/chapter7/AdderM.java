d7559785b1421bf8fb1f5431a11cec89ff6ff9dc
64278001 <64278001@L815WD220020.edu.cbe.ab.ca>
2022-04-29 09:46:52 -0600

    CHAPTER 7 EVERYHTING UPDATED

---
+++ Chapter7/src/chapter7/AdderM.java
@@ -0,0 +1,14 @@
+package chapter7;
+
+public class AdderM {
+	
+	public static int random;
+	public static int random2; 
+	
+	public AdderM()
+	{
+	    random = (int)(10 * Math.random() + 1);
+	}
+
+}
+
