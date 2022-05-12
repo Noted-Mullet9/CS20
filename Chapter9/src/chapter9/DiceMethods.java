d8b9fe301b7db83b1892ed1c222cd6c477405150
64278001 <64278001@L815WD220020.edu.cbe.ab.ca>
2022-05-12 11:58:01 -0600

    Chapter 9 Updated!!!!!!!!!!!!

--- Chapter9/src/chapter9/DiceMethods.java
+++ Chapter9/src/chapter9/DiceMethods.java
@@ -2,15 +2,48 @@ package chapter9;
 
 public class DiceMethods {
 	
-public int[] outcomes = new int[13];
-
-public DiceMethods() {
-
-	int numRolls; 
-	  
-	int outcome; 
-
-  }
+public int[] outcomes = new int[19];
+int numRolls; 
+int outcome; 
 
 
+	public DiceMethods() {
+
+		numRolls = 0;
+		outcome = 0;
+
+	}
+
+	public String askQuestion() {
+	
+		return ("How many rolls?");	
+
+	}
+ 
+	public void setRolls(int v) {
+		  
+		numRolls = v;
+	 
+	}
+
+ 
+ 
+	public void calculate() {
+
+		for (int roll = 0; roll < numRolls; roll++) {
+		  
+			outcome = (int)(6 * Math.random() + 1) + (int)(6 * Math.random() + 1) + (int)(6 * Math.random() + 1);
+		  
+			outcomes[outcome] += 1;
+	 
+		}
+		
+	}
+	
+	public String giveOutcomes(int i) {
+	     
+		return (" " + i + ": " + outcomes[i]);
+			 
+			
+	}
 }
--- Chapter9/src/chapter9/DiceRolls_GUI.java
+++ Chapter9/src/chapter9/DiceRolls_GUI.java
@@ -8,6 +8,9 @@ import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextArea;
 
 public class DiceRolls_GUI {
 
@@ -15,6 +18,8 @@ public class DiceRolls_GUI {
 	
 	DiceMethods dr = new DiceMethods();
 	private JTextField Input;
+	private String output = "";
+	private int p;
 
 	/**
 	 * Launch the application.
@@ -59,23 +64,43 @@ public class DiceRolls_GUI {
 		panel.add(Title);
 		
 		JLabel Prompt1 = new JLabel("sample");
-		Prompt1.setBounds(128, 71, 46, 14);
-		
-		System.out.println("How many rolls? ");
-		dr.outcome = input.nextInt();
-		panel.add(Prompt1);
+		Prompt1.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		Prompt1.setBounds(61, 67, 183, 34);
+		panel.add(Prompt1);				
+	    Prompt1.setText(dr.askQuestion());
 		
 		Input = new JTextField();
-		Input.setBounds(242, 68, 86, 20);
+		Input.setBounds(226, 68, 139, 37);
 		panel.add(Input);
 		Input.setColumns(10);
 		
-		JLabel Outcomes = new JLabel("sample");
-		Outcomes.setBounds(195, 164, 46, 14);
+		JTextArea Outcomes = new JTextArea();
+		Outcomes.setBounds(10, 182, 414, 31);
 		panel.add(Outcomes);
 		
 		JButton Submit_Button = new JButton("Submit");
-		Submit_Button.setBounds(167, 116, 89, 23);
+		Submit_Button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+				p = Integer.parseInt(Input.getText());
+				
+				dr.setRolls(p);
+				
+				dr.calculate();
+				
+				for (int i = 3; i <= 18; i++)
+				{
+					output += (dr.giveOutcomes(i));
+				}
+				
+				Outcomes.setText(output);
+				
+				
+			}
+		});
+		Submit_Button.setBounds(167, 127, 99, 23);
 		panel.add(Submit_Button);
+		
+
 	}
 }
